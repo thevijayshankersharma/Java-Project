110a3b710938b98329ac453d45b7aa26c2d639ab
DarkKnight6 <109781385+DarkKnight6@users.noreply.github.com>
2024-01-26 21:38:34 +0530

    Update DB.java

--- JavaApplication4/src/Library/DB.java
+++ JavaApplication4/src/Library/DB.java
@@ -8,7 +8,7 @@ public class DB {
 		Connection con=null;
 		try{
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Library", "root", "Vijay7569937040");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Library", "root", "Vijay");
 		}catch(Exception e){System.out.println(e);}
 		return con;
 	}
